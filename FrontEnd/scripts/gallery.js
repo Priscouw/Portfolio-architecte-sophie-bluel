a666c168d9e1af8ca0cbe64412a425927b17eb4e
Priscouw <priscouw@gmail.com>
2024-07-18 16:09:30 +0200

    <refactor> refactor galleryjs

--- FrontEnd/scripts/gallery.js
+++ FrontEnd/scripts/gallery.js
@@ -1,80 +1,90 @@
 // Variables globales
 const divGallery = document.querySelector(".gallery");
-const filters = document.querySelector(".filters");
+const divFilters = document.querySelector(".filters");
 
 // Récupération des projets
+async function fetchWorks() {
+  const response = await fetch("http://localhost:5678/api/works");
+  return response.json();
+}
+
+// Récupération des catégories pour boutons
+async function fetchCategories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  return response.json();
+}
+
+// Boucle qui crée les éléments dans la div .gallery
+function afficherProjet(projet) {
+  // Nettoyer la galerie avant d'ajouter de nouveaux projets
+  divGallery.innerHTML = "";
+  for (let i = 0; i < projet.length; i++) {
+    // création des figures
+    const figureGallery = document.createElement("figure");
+    divGallery.appendChild(figureGallery);
+
+    // Images des travaux
+    const imgGallery = document.createElement("img");
+    figureGallery.appendChild(imgGallery);
+    imgGallery.src = projet[i].imageUrl;
+    imgGallery.alt = projet[i].title;
+
+    // Texte des travaux
+    const figcaptionGallery = document.createElement("figcaption");
+    figcaptionGallery.innerText = projet[i].title;
+    figureGallery.appendChild(figcaptionGallery);
+  }
+}
+
+// Créer les boutons de filtres
+function createCategoryButtons(category) {
+  for (let i = 0; i < category.length; i++) {
+    const btn = document.createElement("button");
+    divFilters.appendChild(btn);
+    btn.innerText = category[i].name;
+    btn.dataset.id = category[i].id;
+    btn.classList.add("button");
+  }
+}
+
+// Gérer les événements des boutons de filtres
+function filterButtons(works) {
+  const btnFilters = document.querySelectorAll(".filters .button");
+
+  btnFilters.forEach((btnFilter) => {
+    btnFilter.addEventListener("click", (event) => {
+      const categoryId = event.target.getAttribute("data-id");
+      btnFilters.forEach((btn) => {
+        btn.classList.remove("buttonSelected");
+      });
+
+      // Ajouter la classe 'buttonSelected' au bouton cliqué
+      event.target.classList.add("buttonSelected");
+
+      if (categoryId == 0) {
+        afficherProjet(works);
+      } else {
+        const projetFiltre = works.filter(function (work) {
+          return work.categoryId == categoryId;
+        });
+        afficherProjet(projetFiltre);
+      }
+    });
+  });
+}
+
+// Initialiser l'application
 async function init() {
   try {
-    const works = await fetch("http://localhost:5678/api/works").then((works) =>
-      works.json()
-    );
+    const works = await fetchWorks();
+    afficherProjet(works);
 
-    // Boucle qui créer les éléments dans la div .gallery
+    const categories = await fetchCategories();
+    createCategoryButtons(categories);
 
-    function genererProjet(projet) {
-      for (let i = 0; i < projet.length; i++) {
-        // création des figures
-        const figureGallery = document.createElement("figure");
-        divGallery.appendChild(figureGallery);
-
-        // Images des travaux
-        const imgGallery = document.createElement("img");
-        figureGallery.appendChild(imgGallery);
-        imgGallery.src = projet[i].imageUrl;
-        imgGallery.alt = projet[i].title;
-
-        // Texte des travaux
-        const figcaptionGallery = document.createElement("figcaption");
-        figcaptionGallery.innerText = projet[i].title;
-        figureGallery.appendChild(figcaptionGallery);
-      }
-    }
-
-    genererProjet(works);
-
-    // Récupération des catégories pour boutons
-
-    const categories = await fetch("http://localhost:5678/api/categories").then(
-      (categories) => categories.json()
-    );
-    // boutons filtres
-
-    for (let i = 0; i < categories.length; i++) {
-      const btn = document.createElement("button");
-      filters.appendChild(btn);
-      btn.innerText = categories[i].name;
-      btn.dataset.id = categories[i].id;
-      btn.classList.add("button");
-    }
-
-    const btnFilters = document.querySelectorAll(".filters .button");
-
-    // Au clic de chaque bouton
-
-    btnFilters.forEach((btnFilter) => {
-      btnFilter.addEventListener("click", (event) => {
-        const categoryId = event.target.getAttribute("data-id");
-        btnFilters.forEach((btn) => {
-          btn.classList.remove("buttonSelected");
-        });
-
-        // Ajouter la classe 'buttonSelected' au bouton cliqué
-        event.target.classList.add("buttonSelected");
-
-        if (categoryId == 0) {
-          divGallery.innerHTML = "";
-          genererProjet(works);
-        } else {
-          const projetFiltre = works.filter(function (work) {
-            return work.categoryId == categoryId;
-          });
-          divGallery.innerHTML = "";
-          genererProjet(projetFiltre);
-        }
-      });
-    });
+    filterButtons(works);
   } catch (error) {
-    console.error("erreur : ", error);
+    console.error("Erreur lors de la récupération des données :", error);
   }
 }
 
